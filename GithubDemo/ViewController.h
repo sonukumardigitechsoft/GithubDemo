fe0c5621fc6552ebdaf5e4748148763bca78c67c
sonukumardigitechsoft <sonu.kumar@digitechsoft.com>
2016-06-04 15:15:34 +0530

    Files Uploaded

---
+++ GithubDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubDemo
+//
+//  Created by IOS-DEV on 04/06/16.
+//  Copyright © 2016 LearningInstitute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GithubDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GithubDemo
+//
+//  Created by IOS-DEV on 04/06/16.
+//  Copyright © 2016 LearningInstitute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
